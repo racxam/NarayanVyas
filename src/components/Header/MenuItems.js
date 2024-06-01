79832f0298d2644aedcb285183d3cb8611ebb740
Sumit Jhaldiyal <sumitracxam@gmail.com>
2024-06-01 16:44:55 +0530

    static navigation of header is ready

--- src/components/Header/MenuItems.js
+++ src/components/Header/MenuItems.js
@@ -55,32 +55,43 @@ const MenuItems = (props) => {
         <>
             {/* <li className={parentMenu === 'home' ? 'has-dropdown active' : 'has-dropdown'}><Link to="/">Home</Link> */}
             <li><Link to="/">Home</Link>
-                <ul className={home ? "submenu submenu-open" : "submenu"}>
+                {/* <ul className={home ? "submenu submenu-open" : "submenu"}>
                     <li className={location.pathname === "/" ? "active" : ""}><Link to="/">Creative Agency</Link></li>
                     <li className={location.pathname === "/home-2" ? "active" : ""}><Link to="/home-2">Personal Portfolio</Link></li>
                     <li className={location.pathname === "/home-3" ? "active" : ""}><Link to="/home-3">Startup Business</Link></li>
                     <li className={location.pathname === "/home-4" ? "active" : ""}><Link to="/home-4">Digital Agency</Link></li>
                     <li className={location.pathname === "/home-5" ? "active" : ""}><Link to="/home-5">IT Service Agency</Link></li>
-                </ul>
+                </ul> */}
                 {/* <a href="#" onClick={() => { openMobileMenu('home'); }}></a> */}
             </li>
-            <li><Link to="/about-me">About</Link>
-                {/* <ul className={about ? "submenu submenu-open" : "submenu"}>
-                    <li className={location.pathname === "/about-me" ? "active" : ""}><Link to="/about-me">About Me</Link></li>
-                    <li className={location.pathname === "/about-us" ? "active" : ""}><Link to="/about-us">About US</Link></li>
-                </ul> */}
-                {/* <a href="#" onClick={() => { openMobileMenu('about'); }}></a> */}
+            <li className={parentMenu === 'about-me' ? 'has-dropdown active' : 'has-dropdown'}><Link to="/about-me">About</Link>
+                <ul className={about ? "submenu submenu-open" : "submenu"}>
+                    <li className={location.pathname === "/about-me" ? "active" : ""}><Link to="/about-me">Projects</Link></li>
+                    <li className={location.pathname === "/about-us" ? "active" : ""}><Link to="/appointments">Publications</Link></li>
+                    <li className={location.pathname === "/about-us" ? "active" : ""}><Link to="/appointments">Certificates</Link></li>
+                </ul>
+                <a href="#" onClick={() => { openMobileMenu('about'); }}></a>
+            </li>
+
+            <li className={parentMenu === 'books' ? 'has-dropdown active' : 'has-dropdown'}><Link to="/service-2">Books</Link>
+                <ul className={service ? "submenu submenu-open" : "submenu"}>
+                    <li className={location.pathname === "/service-1" ? "active" : ""}><Link to="/service-1">Pubilshed Books</Link></li>
+                    <li className={location.pathname === "/service-2" ? "active" : ""}><Link to="/service-2">Upcoming Books</Link></li>
+                    <li className={location.pathname === "/service-3" ? "active" : ""}><Link to="/service-3">Call for Chapters</Link></li>
+                    <li className={location.pathname === "/service-details" ? "active" : ""}><Link to="/service-details">Service Details</Link></li>
+                </ul>
+                <a href="#" onClick={() => { openMobileMenu('service'); }}></a>
             </li>
             
-            <li className={parentMenu === 'page' ? 'has-dropdown active' : 'has-dropdown'}><Link to="/#">Books</Link>
+            <li className={parentMenu === 'page' ? 'has-dropdown active' : 'has-dropdown'}><Link to="/project">Services</Link>
                 <ul className={page ? "submenu has-megamenu submenu-open" : "submenu has-megamenu"}>
-                    <li className={location.pathname === "/project" ? "active" : ""}><Link to="/project">Portfolio</Link></li>
-                    <li className={location.pathname === "/project-1" ? "active" : ""}><Link to="/project-1">Portfolio Filter 1</Link></li>
-                    <li className={location.pathname === "/project-2" ? "active" : ""}><Link to="/project-2">Portfolio Filter 2</Link></li>
-                    <li className={location.pathname === "/project-masonary" ? "active" : ""}><Link to="/project-masonary">Portfolio Masonry</Link></li>
-                    <li className={location.pathname === "/project-details" ? "active" : ""}><Link to="/project-details">Portfolio Details</Link></li>
-                    <li className={location.pathname === "/product" ? "active" : ""}><Link to="/product">Product</Link></li>
-                    <li className={location.pathname === "/product-details" ? "active" : ""}><Link to="/product-details">Product Details</Link></li>
+                    <li className={location.pathname === "/project" ? "active" : ""}><Link to="/project">Mobile App Development</Link></li>
+                    <li className={location.pathname === "/project-2" ? "active" : ""}><Link to="/project-2">Research and Development</Link></li>
+                    <li className={location.pathname === "/project-1" ? "active" : ""}><Link to="/project-1">IoT Solutions</Link></li>
+                    <li className={location.pathname === "/project-masonary" ? "active" : ""}><Link to="/project-masonary">Project Consultancy</Link></li>
+                    <li className={location.pathname === "/project-details" ? "active" : ""}><Link to="/project-details">Training & Workshops</Link></li>
+                    <li className={location.pathname === "/product" ? "active" : ""}><Link to="/product"> Publication Assistance</Link></li>
+                    <li className={location.pathname === "/product-details" ? "active" : ""}><Link to="/product-details">Study Notes</Link></li>
                     <li className={location.pathname === "/testimonial" ? "active" : ""}><Link to="/testimonial">Testimonial</Link></li>
                     <li className={location.pathname === "/job" ? "active" : ""}><Link to="/job">Job</Link></li>
                     <li className={location.pathname === "/job-details" ? "active" : ""}><Link to="/job-details">Job Details</Link></li>
@@ -92,18 +103,10 @@ const MenuItems = (props) => {
                     <li className={location.pathname === "/register" ? "active" : ""}><Link to="/register">Register</Link></li>
                     <li className={location.pathname === "*" ? "active" : ""}><Link to="/404">404</Link></li>
                 </ul>
-                <a className={page ? 'mean-expand mean-expand mean-clicked' : 'mean-expand'} href="#" onClick={() => { openMobileMenu('page'); }}><i className="fal fa-plus"></i>hi</a>
+                <a className={page ? 'mean-expand mean-expand mean-clicked' : 'mean-expand'} href="#" onClick={() => { openMobileMenu('page'); }}><i className="fal fa-plus"></i></a>
                 
             </li>
-            <li><Link to="/service-2">Services</Link>
-                {/* <ul className={service ? "submenu submenu-open" : "submenu"}>
-                    <li className={location.pathname === "/service-1" ? "active" : ""}><Link to="/service-1">Service 01</Link></li>
-                    <li className={location.pathname === "/service-2" ? "active" : ""}><Link to="/service-2">Service 02</Link></li>
-                    <li className={location.pathname === "/service-3" ? "active" : ""}><Link to="/service-3">Service 03</Link></li>
-                    <li className={location.pathname === "/service-details" ? "active" : ""}><Link to="/service-details">Service Details</Link></li>
-                </ul> */}
-                {/* <a href="#" onClick={() => { openMobileMenu('service'); }}></a> */}
-            </li>
+            
             {/* <li className={parentMenu === 'blog' ? 'has-dropdown active' : 'has-dropdown'}><Link to="/blog-details">Blog</Link>
                 <ul className={blog ? "submenu submenu-open" : "submenu"}>
                     <li className={location.pathname === "/blog" ? "active" : ""}><Link to="/blog">Blog Sidebar</Link></li>
