ef4a43b8e9d81bcd79860bf4a564afca341ac3e0
Sumit Jhaldiyal <sumitracxam@gmail.com>
2024-06-02 19:38:49 +0530

    correct the overlapping ui problem in the books third time

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -15,12 +15,12 @@ const SingleProduct = (props) => {
                 {/* {Publisher && <div className="product__img-text"><span>{Publisher}</span></div>} */}
 
             </div>
-            <div className="product__content">
+            <div className="product__content d-flex row justify-content-center col align-items-end">
                 <div className="product__content-text">
                     <p className="product__title"><Link to={Url ? Url : ''}>{Title ? Title : 'Creative for mukap'}</Link></p>
                     <div className="product__price" style={{fontWeight:"bold"}} >{Price ? Price : '$555'}</div>
                 </div>
-                <div className="contact__button wow animate__fadeInUp" data-wow-duration="1.1s" style={{marginBottom:"1px"}}>
+                <div className="contact__button wow animate__fadeInUp" data-wow-duration="1.1s" style={{marginBottom:"1px" ,border:"1px solid black"}} >
                     <a className="main-btn-sm  tp-btn-hover alt-color" href={Url ? Url : ''}><span>View Details</span><b></b></a>
                 </div>
             </div>
