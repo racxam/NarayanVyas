9b54ed7153e2a5da7fba1f87eb3091a4d66d9784
Sumit Jhaldiyal <sumitracxam@gmail.com>
2024-06-02 19:15:50 +0530

    added the books component to the home page

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -7,7 +7,7 @@ const SingleProduct = (props) => {
 
         
 
-        <div className={itemClass ? itemClass : 'product__item text-center'}>
+        <div className={itemClass ? itemClass : 'product__item text-center'}>  
             <div className="product__img-container">
                 <Link to={Url ? Url : ''}>
                     <img src={Image ? Image : productImg} alt={Title ? Title : 'Book Image'} className="product__img" />
@@ -17,8 +17,8 @@ const SingleProduct = (props) => {
             </div>
             <div className="product__content">
                 <div className="product__content-text">
-                    <h6 className="product__title"><Link to={Url ? Url : ''}>{Title ? Title : 'Creative for mukap'}</Link></h6>
-                    <span className="product__price">{Price ? Price : '$555'}</span>
+                    <p className="product__title"><Link to={Url ? Url : ''}>{Title ? Title : 'Creative for mukap'}</Link></p>
+                    <span className="product__price" style={{fontWeight:"bold"}} >{Price ? Price : '$555'}</span>
                 </div>
                 <div className="contact__button wow animate__fadeInUp" data-wow-duration="1.1s">
                     <a className="main-btn-sm  tp-btn-hover alt-color" href={Url ? Url : ''}><span>View Details</span><b></b></a>
