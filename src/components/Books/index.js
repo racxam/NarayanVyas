05d876965780e949b25ee43dd646e80c1abb607c
Sumit Jhaldiyal <sumitracxam@gmail.com>
2024-06-02 20:52:34 +0530

    added pubished book heading in the home page section

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SingleProduct from '../../components/Product';
-
+import SectionTitle from '../../components/SectionTitle/index'
 import productImg1 from '../../assets/img/books/few-shot-zero-shot-igi.jpg';
 import productImg2 from '../../assets/img/books/ai-driven-alzheimer-igi.jpg';
 import productImg3 from '../../assets/img/books/quantam-for-biomedical-igi.jpg';
@@ -10,12 +10,18 @@ const index = () => {
   return (
     <div className="product__area product__plr mt-100 mb-70">
     <div className="container-fluid">
-        <div className="row">
-            <div className="col-xl-12">
-                <div className="product__title-sm wow animate__fadeInUp" data-wow-duration="1.1s">
-                    <h4>Showing All Published Books</h4>
+    <div className="col-xl-12 col-lg-8 wow animate__fadeIn mt-100" data-wow-duration="1.1s">
+            <div className="row justify-content-center">
+                <div className="col-xl-6 col-lg-6 wow animate__fadeInUp" data-wow-duration="1.1s">
+                    <SectionTitle
+                        sectionClass="service__section-box text-center pb-35 section-title-fixed-width"
+                        subTitle="Published Books"
+                        Title="Showing All Published Books"
+                    />
                 </div>
             </div>
+
+
         </div>
         <div className="row">
             <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-30 wow animate__fadeInUp" data-wow-duration="1.1s">
