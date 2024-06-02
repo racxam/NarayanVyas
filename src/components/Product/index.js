04c8d29a69a53d25ad81e3179e280e758675f6c6
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-06-02 18:42:20 +0530

    Fixed Boox Sized

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,36 +1,27 @@
 import { Link } from 'react-router-dom';
-
-import productImg from '../../assets/img/product/product-3.jpg'
+import productImg from '../../assets/img/product/product-3.jpg';
 
 const SingleProduct = (props) => {
     const { itemClass, Image, Title, Price, Publisher, Url, btnText } = props;
     return (
         <div className={itemClass ? itemClass : 'product__item text-center'}>
-            <div className="product__img">
-                <img src={Image ? Image : productImg} alt="" />  
-                
-                {/* It was for the name of the publisher  */}
-                {/* <div className="product__img-text">
-                    <span>{Publisher ? Publisher : ''}</span>
-                </div> */}
-
-
-                {/* <div className="product__icon">
-                    <Link to={Url ? Url : ''}><i className="fal fa-shopping-basket"></i></Link>
-                    <Link to={Url ? Url : ''}><i className="fal fa-heart"></i></Link>
-                </div> */}
+            <div className="product__img-container">
+                <Link to={Url ? Url : ''}>
+                    <img src={Image ? Image : productImg} alt={Title ? Title : 'Book Image'} className="product__img" />
+                </Link>
+                {/* {Publisher && <div className="product__img-text"><span>{Publisher}</span></div>} */}
             </div>
             <div className="product__content">
                 <div className="product__content-text">
-                    <h6><Link to={Url ? Url : ''}>{Title ? Title : 'Creative for mukap'}</Link></h6>
-                    <span>{Price ? Price : '$555'}</span>
+                    <h6 className="product__title"><Link to={Url ? Url : ''}>{Title ? Title : 'Creative for mukap'}</Link></h6>
+                    <span className="product__price">{Price ? Price : '$555'}</span>
                 </div>
-                <div className="product__button">
-                    <a className="yellow-btn-sm" href={Url ? Url : ''}>{btnText ? btnText : 'View Details'}</a>
+                <div className="contact__button wow animate__fadeInUp" data-wow-duration="1.1s">
+                    <a className="main-btn-sm  tp-btn-hover alt-color" href={Url ? Url : ''}><span>View Details</span><b></b></a>
                 </div>
             </div>
         </div>
     )
 }
 
-export default SingleProduct
+export default SingleProduct;
