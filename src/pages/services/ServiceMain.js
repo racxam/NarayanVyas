41419f091cb46eb246b7d8baa5a7de46c7933671
Narayan Vyas <narayanvyas87@gmail.com>
2024-06-04 07:49:42 +0530

    Minor bug fix in service page

--- src/pages/services/ServiceMain.js
+++ src/pages/services/ServiceMain.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import servicesData from '../../data/servicesData';
-import SectionTitle from '../../components/SectionTitle';
 import SingleService from '../../components/Service';
-import shapeImg1 from '../../assets/img/service/sv-shape-1.png';
-import shapeImg2 from '../../assets/img/service/sv-shape-2.png';
 
 const ServiceMain = () => {
 	return (
-		<div className="service__area service__space service__mlr grey-bg pt-100 pb-70 p-relative mt-1">
-			<div className="service__shape-1">
-				<img src={shapeImg1} alt="" />
-			</div>
-			<div className="service__shape-2">
-				<img src={shapeImg2} alt="" />
-			</div>
+		<div className="service__area service__space service__mlr pt-100 pb-70 p-relative mt-1">
 			<div className="container-fluid">
-				<div className="row justify-content-center">
-					<div className="col-xl-6 col-lg-6 wow animate__fadeInUp" data-wow-duration="1.1s">
-						<SectionTitle
-							sectionClass="service__section-box text-center pb-35 section-title-fixed-width"
-							subTitle="My Services"
-							Title="Experts in Every Aspect of Technology and Research"
-						/>
-					</div>
-				</div>
 				<div className="row">
 					{servicesData.map((service, index) => {
 						const colorIndex = (index % 6) + 1; // Ensures the color index loops between 1 and 6
