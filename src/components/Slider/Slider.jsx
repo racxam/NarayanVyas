f00facb87b2c1c4872671774abad908d3091c7c1
Sumit Jhaldiyal <sumitracxam@gmail.com>
2024-06-05 20:47:38 +0530

    just now added the slider realted change via a git merge

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -38,13 +38,13 @@ function Slider1() {
           modifier: 2.5,
 
         }}
-          slidesPerView={'auto'}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
+          // // slidesPerView={'auto'}
+          // coverflowEffect={{
+          //   rotate: 0,
+          //   stretch: 0,
+          //   depth: 100,
+          //   modifier: 2.5,
+          // }}
          
           pagination={{ el: '.swiper-pagination1', clickable: true }}
           navigation={{
