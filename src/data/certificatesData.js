e89fa8581820c7c70f2ce25265ed8c124f872c05
Narayan Vyas <narayanvyas87@gmail.com>
2024-06-06 15:08:29 +0530

    minor bug fix

--- src/data/certificatesData.js
+++ src/data/certificatesData.js
@@ -3,7 +3,7 @@ import images from '../assets/images';
 const certificatesData = [
     {
         id: "icaiihi_2023_session_chair",
-        image: images.certificates.icaiihi_2023_session_chair,
+        image: images.certificates.awards.icaiihi_2023_session_chair,
         title: "ICAIHI 2023 Session Chair",
         issueDate: "2023-05-15",
         certificateId: "ICAIHI2023-001",
@@ -12,7 +12,7 @@ const certificatesData = [
     },
     {
         id: "ssn_college_workshop_2021",
-        image: images.certificates.ssn_college_workshop_2021,
+        image: images.certificates.awards.ssn_college_workshop_2021,
         title: "SSN College Workshop 2021",
         issueDate: "2021-03-10",
         certificateId: "SSN2021-002",
@@ -21,7 +21,7 @@ const certificatesData = [
     },
     {
         id: "icdsns_2023_reviewer",
-        image: images.certificates.icdsns_2023_reviewer,
+        image: images.certificates.awards.icdsns_2023_reviewer,
         title: "ICDSNS 2023 Reviewer",
         issueDate: "2023-01-25",
         certificateId: "ICDSNS2023-003",
@@ -30,7 +30,7 @@ const certificatesData = [
     },
     {
         id: "nimitcon_2023_reviewer",
-        image: images.certificates.nimitcon_2023_reviewer,
+        image: images.certificates.awards.nimitcon_2023_reviewer,
         title: "NimitCon 2023 Reviewer",
         issueDate: "2023-02-17",
         certificateId: "NIMITCON2023-004",
@@ -39,7 +39,7 @@ const certificatesData = [
     },
     {
         id: "ieee_xtreme_proctor",
-        image: images.certificates.ieee_xtreme_proctor,
+        image: images.certificates.awards.ieee_xtreme_proctor,
         title: "IEEE Xtreme Proctor",
         issueDate: "2022-10-19",
         certificateId: "IEEE2022-005",
@@ -48,7 +48,7 @@ const certificatesData = [
     },
     {
         id: "ieee_membership",
-        image: images.certificates.ieee_membership,
+        image: images.certificates.memberships.ieee_membership,
         title: "IEEE Membership",
         issueDate: "2023-01-01",
         certificateId: "IEEE2023-006",
@@ -57,7 +57,7 @@ const certificatesData = [
     },
     {
         id: "acm_membership",
-        image: images.certificates.acm_membership,
+        image: images.certificates.memberships.acm_membership,
         title: "ACM Membership",
         issueDate: "2023-02-15",
         certificateId: "ACM2023-007",
@@ -66,7 +66,7 @@ const certificatesData = [
     },
     {
         id: "grss_membership",
-        image: images.certificates.grss_membership,
+        image: images.certificates.memberships.grss_membership,
         title: "GRSS Membership",
         issueDate: "2023-03-20",
         certificateId: "GRSS2023-008",
@@ -75,7 +75,7 @@ const certificatesData = [
     },
     {
         id: "ieee_young_professional",
-        image: images.certificates.ieee_young_professional,
+        image: images.certificates.memberships.ieee_young_professional,
         title: "IEEE Young Professional",
         issueDate: "2023-04-05",
         certificateId: "IEEE2023-009",
@@ -84,7 +84,7 @@ const certificatesData = [
     },
     {
         id: "ieee_consultant_network",
-        image: images.certificates.ieee_consultant_network,
+        image: images.certificates.memberships.ieee_consultant_network,
         title: "IEEE Consultant Network",
         issueDate: "2023-05-25",
         certificateId: "IEEE2023-010",
@@ -93,7 +93,7 @@ const certificatesData = [
     },
     {
         id: "coursera_blockchain",
-        image: images.certificates.coursera_blockchain,
+        image: images.certificates.courses.coursera_blockchain,
         title: "Coursera Blockchain",
         issueDate: "2022-06-10",
         certificateId: "COURSERA2022-011",
@@ -102,7 +102,7 @@ const certificatesData = [
     },
     {
         id: "innobuzz_ceh",
-        image: images.certificates.innobuzz_ceh,
+        image: images.certificates.courses.innobuzz_ceh,
         title: "Innobuzz CEH",
         issueDate: "2021-07-15",
         certificateId: "INNOBUZZ2021-012",
@@ -111,7 +111,7 @@ const certificatesData = [
     },
     {
         id: "udemy_arduino",
-        image: images.certificates.udemy_arduino,
+        image: images.certificates.courses.udemy_arduino,
         title: "Udemy Arduino",
         issueDate: "2021-08-20",
         certificateId: "UDEMY2021-013",
@@ -120,7 +120,7 @@ const certificatesData = [
     },
     {
         id: "udemy_ethereum_solidity",
-        image: images.certificates.udemy_ethereum_solidity,
+        image: images.certificates.courses.udemy_ethereum_solidity,
         title: "Udemy Ethereum & Solidity",
         issueDate: "2021-09-25",
         certificateId: "UDEMY2021-014",
@@ -129,7 +129,7 @@ const certificatesData = [
     },
     {
         id: "udemy_flutter",
-        image: images.certificates.udemy_flutter,
+        image: images.certificates.courses.udemy_flutter,
         title: "Udemy Flutter",
         issueDate: "2021-10-30",
         certificateId: "UDEMY2021-015",
@@ -138,7 +138,7 @@ const certificatesData = [
     },
     {
         id: "sololearn_html",
-        image: images.certificates.sololearn_html,
+        image: images.certificates.courses.sololearn_html,
         title: "Sololearn HTML",
         issueDate: "2021-11-05",
         certificateId: "SOLOLEARN2021-016",
@@ -147,7 +147,7 @@ const certificatesData = [
     },
     {
         id: "sololearn_css",
-        image: images.certificates.sololearn_css,
+        image: images.certificates.courses.sololearn_css,
         title: "Sololearn CSS",
         issueDate: "2021-12-10",
         certificateId: "SOLOLEARN2021-017",
@@ -156,7 +156,7 @@ const certificatesData = [
     },
     {
         id: "udemy_kicad",
-        image: images.certificates.udemy_kicad,
+        image: images.certificates.courses.udemy_kicad,
         title: "Udemy KiCad",
         issueDate: "2022-01-15",
         certificateId: "UDEMY2022-018",
@@ -165,7 +165,7 @@ const certificatesData = [
     },
     {
         id: "udemy_raspberry_pi",
-        image: images.certificates.udemy_raspberry_pi,
+        image: images.certificates.courses.udemy_raspberry_pi,
         title: "Udemy Raspberry Pi",
         issueDate: "2022-02-20",
         certificateId: "UDEMY2022-019",
@@ -174,7 +174,7 @@ const certificatesData = [
     },
     {
         id: "vskills_html5",
-        image: images.certificates.vskills_html5,
+        image: images.certificates.courses.vskills_html5,
         title: "Vskills HTML5",
         issueDate: "2022-03-25",
         certificateId: "VSKILLS2022-020",
